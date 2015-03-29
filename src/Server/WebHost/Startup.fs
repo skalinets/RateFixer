06b07bba84fbef8cccc0db992903538784930f14
Serhiy Kalinets <kalinets@gmail.com>
2015-03-29 18:51:06 +0200

    webapi is slmost there...

---
+++ src/Server/WebHost/Startup.fs
@@ -0,0 +1,34 @@
+﻿namespace WebHost
+
+open Owin
+open Microsoft.Owin
+open Microsoft.Owin.Host
+open System.Web.Http
+
+type Startup() =
+
+    member x.Configuration(app: Owin.IAppBuilder) =
+      let cfg = new HttpConfiguration()
+      cfg.Routes.MapHttpRoute(
+          name = "Default", 
+          routeTemplate = "api/{controller}/{id}", 
+          defaults = RouteParameter.Optional) 
+        |> ignore
+      
+      app
+        .UseWebApi(cfg)
+        .MapSignalR()
+        .Run(fun ctx ->
+          ctx.Response.ContentType <- "text/plain"
+          ctx.Response.WriteAsync("Web Application Server with F#, Owin, ASP.NET Identity, SignalR...")
+      ) |> ignore
+
+type Response = {Name: string; Message: string}    
+type RateController() = 
+  inherit ApiController() 
+  member __.Get(id) = 
+    { Name = id; Message = "I am watching you" } 
+  
+
+[<assembly: OwinStartup(typeof<Startup>)>]
+do ()
